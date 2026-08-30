ebcb08b0b86157236b9eb8bc3f10c84bf9652210
Abdullah Ali Khan <m.abdullahali987@gmail.com>
2026-08-30 22:08:50 +0500

    Fix Laravel Vercel runtime entrypoint

--- api/index.php
+++ api/index.php
@@ -1,3 +1,17 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$request = Request::capture();
+
+$response = $app->handleRequest($request);
+
+$response->send();
+
+$app->terminate($request, $response);
